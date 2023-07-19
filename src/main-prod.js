725f08b66d0f70d1dd6e5aa4d8478d8169a557e1
ueaoZJ <1090780023@qq.com>
2023-07-19 18:42:21 +0900

    通过修改webpack为生产和开发hu环境配置不同的入口文件

---
+++ src/main-prod.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+import './plugins/element'
+import TreeTable from 'vue-table-with-tree-grid'
+
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
+Vue.config.productionTip = false
+
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config=>{
+  NProgress.start();
+  config.headers.Authorization = window.sessionStorage.getItem("token");
+  return config;
+})
+axios.interceptors.response.use(config=>{
+  NProgress.done()
+  return config;
+})
+Vue.prototype.$http = axios
+
+
+Vue.component('tree-table', TreeTable)
+Vue.use(VueQuillEditor, /* { default global options } */)
+
+Vue.filter("dateFilter",(str)=>{
+  let dt = new Date(str);
+  let yy = dt.getFullYear();
+  let mm = (dt.getMonth()+1+'').padStart(2,'0');
+  let dd = (dt.getDate()+'').padStart(2,'0');
+  
+  let hh = (dt.getHours()+'').padStart(2,'0');
+  let MM = (dt.getMinutes()+'').padStart(2,'0');
+  let ss = (dt.getSeconds()+'').padStart(2,'0');
+  return `${yy}-${mm}-${dd} ${hh}-${MM}-${ss}`;
+})
+
+
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 通过chainWebpack自定义打包入口(默认是共用main.js)
+  chainWebpack:config=>{
+    config.when(process.env.NODE_ENV === 'production',config=>{
+      config.entry('app').clear().add('./src/main-prod.js')
+    })
+    config.when(process.env.NODE_ENV === 'development',config=>{
+      config.entry('app').clear().add('./src/main-dev.js')
+    })
+  }
 })
